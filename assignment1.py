a0d6a76901e4cf83ca2c94721841cf365010ed4e
Páll H. Rasmussen <2023.298@student.setur.fo>
2023-10-30 11:01:08 +0000

    t4

--- assignment1.py
+++ assignment1.py
@@ -1,15 +1,18 @@
-
-
-s=int(input("Skriva eitt fimm siffrað positivt heiltal:"))
-
-if s < 10000:
-	print('Skriva eitt positiv fimmsifrað heiltal')
+"""Innlating 1"""
+try:
+	s=int(input("Skriva eitt fimm siffrað positivt heiltal:"))
+except:
+	print('Error')
 else:
-	s = str(s)
-	for i in s.split():
-		j = '   '.join(i)
-		#print('   '.join(i))
-		print(s + ' >> ' + j)
+
+	if s < 10000:
+		print('Skriva eitt positiv fimmsifrað heiltal')
+	else:
+		s = str(s)
+		for i in s.split():
+			j = '   '.join(i)
+			#print('   '.join(i))
+			print(s + ' >> ' + j)
 
 
 #s=input("Skriva eitt fimm siffrað positivt heiltal:")
