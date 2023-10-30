78a96c2171e59eee60a143e4ea8ce47c98667924
Páll H. Rasmussen <2023.298@student.setur.fo>
2023-10-30 13:10:58 +0000

    t6

--- exercise2_12.py
+++ exercise2_12.py
@@ -1,20 +1,20 @@
 """Exercise 2.12"""
-# Formula a = p(1 + r) ** n
+# Formula a = p(1 + r) ** n where p is start amount, r is interrest rate and n periode
 
-p = 1000.0
+p = 1000.00
 r = 0.07
 n = 10
 
-a = p * r**n
+a = p * (1 + r)**n
 
 print(a)
 
 n = n * 2
-a = p * r**n
+a = p * (1 + r)**n
 
 print(a)
 
 n = n * 3
-a = p * r**n
+a = p * (1 + r)**n
 
 print(a)
