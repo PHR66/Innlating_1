7b0a6686c22552e57b5a66d63d6273eeab9d5c98
Páll H. Rasmussen <2023.298@student.setur.fo>
2023-10-30 12:16:25 +0000

    i2

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,7 @@
 """Innlating 1"""
 
-number = int(input("Skriva eitt positivt heiltal ímillum 10000 og 99999 "))
+number = int(input("Skriva eitt heiltal ímillum 10000 og 99999 "))
 if number >= 10000 and number < 100000:
 	print(number // 10000, '   ', number % 10000 // 1000, '   ', number % 1000 // 100, '   ', number % 100 // 10, '   ', number % 10)
 else:
-	print("Skriva eitt positivt heiltal ímillum 10000 og 99999 ")
+	print("Skriva eitt heiltal ímillum 10000 og 99999 ")
